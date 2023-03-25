00cb089ba43d395dbd4d9b4dc7071821066594d9
JorgeM28 <jorge2224764@correo.uis.edu.co>
2023-03-24 20:04:39 -0500

    Estructura

---
+++ vista/VentanaPrincipal.java
@@ -0,0 +1,14 @@
+package vista;
+
+import javax.swing.JFrame;
+
+public class VentanaPrincipal extends JFrame
+{
+    //----------------------
+    //Atributos
+    //----------------------
+    public PanelEntradaDatos miPanelEntradaDatos;
+    public PanelOperaciones miPanelOperaciones;
+    public PanelResultados miPanelResultado;
+    
+}
